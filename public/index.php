4639fa85524a212a37abe53d2dea1de1b57ae34b
Sylvain T <garvek@gmail.com>
2021-10-12 09:06:21 +0200

    Support for CodeAnywhere

--- public/index.php
+++ public/index.php
@@ -4,6 +4,12 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// https://github.com/symfony/symfony/issues/37980
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+    $_SERVER['HTTPS'] = 'on';
+    $_SERVER['SERVER_PORT'] = 443;
+}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
